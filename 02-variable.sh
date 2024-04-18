626e05a3136e97896ea9cdbc959f4a5d7682142f
unknown <madhusagardasari@gmail.com>
2024-04-18 13:32:11 +0800

    xxx

---
+++ 02-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+### user name and password printing
+echo ("please enter username:")
+read -s USERNAME
+
+echo ("pease enter password:")
+read -s PASSWORD
+
+echo ("your username and password is:$USERNAME $PASSWORD")
